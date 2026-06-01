8c4f160673e6c61943e3e9ea71476c205afcec61
Mário Pina <86622495+mfopina@users.noreply.github.com>
2026-06-01 17:02:57 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,4 @@
-// =========================
-// Firebase Configuration
-// =========================
+// firebase.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 
@@ -8,23 +6,36 @@ import {
     getFirestore
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
-// ======================================================
-// SUBSTITUA PELOS DADOS DO SEU PROJETO FIREBASE
-// ======================================================
+import {
+    getAnalytics
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
+
+// =====================================
+// CONFIGURAÇÃO DO FIREBASE
+// =====================================
 
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    projectId: "SEU_PROJETO",
-    storageBucket: "SEU_PROJETO.appspot.com",
-    messagingSenderId: "000000000000",
-    appId: "SEU_APP_ID"
+    apiKey: "AIzaSyBwd3e6wneYAOULHDBAwhq3mraHTPWO7i0",
+    authDomain: "batalha-naval-online-ed013.firebaseapp.com",
+    projectId: "batalha-naval-online-ed013",
+    storageBucket: "batalha-naval-online-ed013.firebasestorage.app",
+    messagingSenderId: "848215303165",
+    appId: "1:848215303165:web:c30c20a48e4714deb9ef94",
+    measurementId: "G-7ZTY2C3Y17"
 };
 
-// ======================================================
+// =====================================
+// INICIALIZAÇÃO
+// =====================================
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-export { db };
+const analytics = getAnalytics(app);
+
+// =====================================
+// EXPORTAÇÕES
+// =====================================
+
+export { db, app, analytics };
