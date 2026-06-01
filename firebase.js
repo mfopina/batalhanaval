024e7e1d1646362249abc71722af0cae3a3b9c6e
Mário Pina <86622495+mfopina@users.noreply.github.com>
2026-06-01 18:28:47 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,53 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
-const firebaseConfig={};
-const app=initializeApp(firebaseConfig);
-export const db=getFirestore(app);
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+// =====================================
+// CONFIGURAÇÃO FIREBASE
+// =====================================
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBwd3e6wneYAOULHDBAwhq3mraHTPWO7i0",
+
+    authDomain:
+    "batalha-naval-online-ed013.firebaseapp.com",
+
+    projectId:
+    "batalha-naval-online-ed013",
+
+    storageBucket:
+    "batalha-naval-online-ed013.firebasestorage.app",
+
+    messagingSenderId:
+    "848215303165",
+
+    appId:
+    "1:848215303165:web:c30c20a48e4714deb9ef94"
+
+};
+
+// =====================================
+// INICIALIZA FIREBASE
+// =====================================
+
+const app =
+initializeApp(firebaseConfig);
+
+// =====================================
+// FIRESTORE
+// =====================================
+
+const db =
+getFirestore(app);
+
+// =====================================
+// EXPORTAÇÕES
+// =====================================
+
+export {
+    app,
+    db
+};
